5af8c3571244a23a873573c0c37a0e9d5d16180d
Joseph Kinuthia <cjkinuthia5@gmail.com>
2024-05-22 14:55:39 +0300

    added gemini,implemented sending summary to frontend

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-import cors from "cors";
-import RSSparser from "rss-parser";
-import express from "express";
+const cors = require("cors")
+const RSSparser = require("rss-parser")
+const express = require("express")
 
+
+const genAI = require("./gemini-start")
+const config = require("./utils/config")
 
 const feedURLs = [
     {   
@@ -49,11 +52,39 @@ parseFeeds()
 
 const app = express();
 app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send(feedsData)
 })
 
-const server = app.listen('3001', () => {
+
+app.post('/summaries', async(req, res) => {
+    const { actualUrl } = req.body
+    const model = genAI.getGenerativeModel({ model: "gemini-pro"})
+
+    const prompt = `Summarize this blog: ${actualUrl}`
+
+    const result = await model.generateContent(prompt)
+    const response = await result.response
+    const text = response.text()
+    res.send(text)
+})
+
+async function run() {
+    const model = genAI.getGenerativeModel({ model: "gemini-pro"})
+
+    const prompt = "Summarize this blog: https://blog.bytebytego.com/p/cloudflares-trillion-message-kafka"
+
+    const result = await model.generateContent(prompt)
+    const response = await result.response
+    const text = response.text()
+    console.log(text);
+}
+
+// run()
+
+
+const server = app.listen(config.PORT, () => {
     console.log('server running');
 })
